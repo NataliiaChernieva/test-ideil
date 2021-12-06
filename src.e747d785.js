487d58a5fcc0de6cc1f8eccf531c640a869e31bc
NataliiaChernieva <74625141+NataliiaChernieva@users.noreply.github.com>
2021-12-06 21:40:46 +0000

    Deploying to gh-pages from @ NataliiaChernieva/test-ideil@0bfca0f2fcab4767bb447d48e1a7b3c028d1ccd3 🚀

--- src.e747d785.js
+++ src.e747d785.js
@@ -42,4 +42,4 @@ var e,t=arguments[3];!function(t,r){"object"==typeof exports&&"object"==typeof m
 },{"../tamplates/sec-tmp.hbs":"SRc3"}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss"),require("./js/burger"),require("./js/search"),require("./js/sec1"),require("./js/sec2"),require("./js/big-water"),require("./js/range"),require("./js/ukraine"),require("./js/popularNow"),require("./js/beauty-country");
 },{"./sass/main.scss":"clu1","./js/burger":"toNQ","./js/search":"HU2o","./js/sec1":"UpPB","./js/sec2":"Syb1","./js/big-water":"ySAL","./js/range":"A8Cw","./js/ukraine":"cFOG","./js/popularNow":"BaIA","./js/beauty-country":"GMcQ"}]},{},["Focm"], null)
-//# sourceMappingURL=/src.23d4569e.js.map
+//# sourceMappingURL=/src.e747d785.js.map

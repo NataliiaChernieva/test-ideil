57a6528019d6dfcf7b5f245c19657816b1510239
NataliiaChernieva <NataliiaChernieva@gmail.com>
2021-12-09 21:23:49 +0200

    first slider

--- src/js/glideSlider.js
+++ src/js/glideSlider.js
@@ -1,16 +1,16 @@
-import Glide from '@glidejs/glide/dist/glide.modular.esm'
+// import Glide from '@glidejs/glide/dist/glide.modular.esm'
 
-const focus = new Glide('.long-focus', {
-          type: 'carousel',
-          autoplay: 3000,
-          perView: 1,
-        }).mount();
-const range = new Glide('.range-slider', {
-          type: 'carousel',
-          perView: 3,
-        }).mount();
-  new Glide('.popular-now', {
-          type: 'carousel',
-          perView: 4,
-          gap: 30,
-        }).mount();
+// const focus = new Glide('.long-focus', {
+//           type: 'carousel',
+//           autoplay: 3000,
+//           perView: 1,
+//         }).mount();
+// const range = new Glide('.range-slider', {
+//           type: 'carousel',
+//           perView: 3,
+//         }).mount();
+//   new Glide('.popular-now', {
+//           type: 'carousel',
+//           perView: 4,
+//           gap: 30,
+//         }).mount();
--- src/js/sec1.js
+++ src/js/sec1.js
@@ -1,19 +1,21 @@
-import firstSectionTemplate from '../tamplates/sec1-tmp.hbs'
+// import firstSectionTemplate from '../tamplates/sec1-tmp.hbs'
 
-const slidesFirstContainer = document.querySelector('.first-container-js')
-const mainSlide = document.querySelector('.slider-for');
+// const slidesFirstContainer = document.querySelector('.first-container-js')
+// const mainSlide = document.querySelector('.slider-for');
 
-const renderFirstSectionMarkup = data => {
-    mainSlide.insertAdjacentHTML('afterbegin', "")
-    const markup = firstSectionTemplate(data);
-    mainSlide.insertAdjacentHTML('afterbegin', markup)
-}
+// const renderFirstSectionMarkup = data => {
+//     mainSlide.insertAdjacentHTML('afterbegin', "")
+//     const markup = firstSectionTemplate(data);
+//     mainSlide.insertAdjacentHTML('afterbegin', markup)
+// }
 
 // fetch('http://localhost:3000/sec1').then(res=>res.json()).then(renderFirstSectionMarkup)
 
-slidesFirstContainer.addEventListener('click', function (e){
-    // if (e.target.nodeName !== 'div'|| e.target.nodeName !== 'h2' ) return;
-    console.log(`object`, e.target.id)
-    const id = e.target.id;
-    fetch(`http://localhost:3000/sec1/${id}`).then(res => res.json()).then(renderFirstSectionMarkup);
-})
+// slidesFirstContainer.addEventListener('click', function (e){
+//     // if (e.target.nodeName !== 'div'|| e.target.nodeName !== 'h2' ) return;
+//     console.log(`object`, e.target.id)
+//     const id = e.target.id;
+//     fetch(`http://localhost:3000/sec1/${id}`).then(res => res.json()).then(renderFirstSectionMarkup);
+// })
+
+

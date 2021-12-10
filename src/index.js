c88ab276d22c5f4a00a081b031b96444a5ae534a
NataliiaChernieva <NataliiaChernieva@gmail.com>
2021-12-10 18:06:49 +0200

    corect

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import './js/sec1';
 import './js/sec2';
 import './js/big-water';
 import './js/sec4';
-import './js/range';
+// import './js/range';
 import './js/ukraine';
-import './js/popularNow';
+// import './js/popularNow';
 import './js/beauty-country';
 
 // import './js/glideSlider';
